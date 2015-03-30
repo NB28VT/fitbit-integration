7ec35544f5b4efd6b5af737f33c4337a2d9d07e7
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-03-30 17:48:55 -0400

    Feature complete

--- spec/features/dashboard/sign_into_fitbit_spec.rb
+++ spec/features/dashboard/sign_into_fitbit_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 feature "A user can get into their Fitbit account" do
-  scenario "A user signs in with a valid fitbit code" do
-    visit dashboard_index_path
+    scenario "A user signs in with a valid fitbit code" do
+      visit dashboard_index_path
 
-    click_on "Link to fitbit validation"
+      click_on "Link to fitbit validation"
 
-    expect(page).to have_content("Enter code")
-  end
-
+      expect(page).to have_content("Enter code")
+    end
   end
 end
